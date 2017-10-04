1924fa222dc26ed091b4e317846972b367c048c5
scott <wwx7266@hotmail.com>
2017-10-04 17:11:48 +1100

    start up code

---
+++ hw2sent/implementation.py
@@ -0,0 +1,31 @@
+<HTML>
+<HEAD>
+<TITLE>CGIWrap Error: Execution of this script not permitted</TITLE>
+<CENTER><H2>CGIWrap Error: Execution of this script not permitted</H2></CENTER>
+<HR><p></HEAD><BODY>
+Execution of (17s2/hw2/src/hw2sent/implementation.py) is not permitted
+for the following reason:
+
+<P><DL><DT>Script is not executable. Issue 'chmod 755 filename'</DL>
+<P>
+<DL>
+<DT><B>Server Data:</B>
+<P>
+<DD><B>Server Administrator/Contact</B>: ss@cse.unsw.edu.au
+<DD><B>Server Name</B>: cgi.cse.unsw.edu.au
+<DD><B>Server Port</B>: 80
+<DD><B>Server Protocol</B>: HTTP/1.1
+<DD><B>Virtual Host</B>: cgi.cse.unsw.edu.au
+</DL>
+<P>
+<DL>
+<DT><B>Request Data:</B>
+<P>
+<DD><B>User Agent/Browser</B>: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36
+<DD><B>Request Method</B>: GET
+<DD><B>Remote Address</B>: 129.94.8.246
+<DD><B>Remote Port</B>: 32324
+<DD><B>Referring Page</B>: http://www.cse.unsw.edu.au/~cs9444/17s2/hw2/src/hw2sent/
+</DL>
+
+</BODY></HTML>
