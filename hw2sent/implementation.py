e029eff84e4d9310b54dc8b2e2a680d4620a8bca
scott <wwx7266@hotmail.com>
2017-10-10 16:19:26 +1100

    change python file

--- hw2sent/implementation.py
+++ hw2sent/implementation.py
@@ -1,31 +1,51 @@
-<HTML>
-<HEAD>
-<TITLE>CGIWrap Error: Execution of this script not permitted</TITLE>
-<CENTER><H2>CGIWrap Error: Execution of this script not permitted</H2></CENTER>
-<HR><p></HEAD><BODY>
-Execution of (17s2/hw2/src/hw2sent/implementation.py) is not permitted
-for the following reason:
+import tensorflow as tf
+import numpy as np
+import glob #this will be useful when reading reviews from file
+import os
+import tarfile
 
-<P><DL><DT>Script is not executable. Issue 'chmod 755 filename'</DL>
-<P>
-<DL>
-<DT><B>Server Data:</B>
-<P>
-<DD><B>Server Administrator/Contact</B>: ss@cse.unsw.edu.au
-<DD><B>Server Name</B>: cgi.cse.unsw.edu.au
-<DD><B>Server Port</B>: 80
-<DD><B>Server Protocol</B>: HTTP/1.1
-<DD><B>Virtual Host</B>: cgi.cse.unsw.edu.au
-</DL>
-<P>
-<DL>
-<DT><B>Request Data:</B>
-<P>
-<DD><B>User Agent/Browser</B>: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36
-<DD><B>Request Method</B>: GET
-<DD><B>Remote Address</B>: 129.94.8.246
-<DD><B>Remote Port</B>: 32324
-<DD><B>Referring Page</B>: http://www.cse.unsw.edu.au/~cs9444/17s2/hw2/src/hw2sent/
-</DL>
 
-</BODY></HTML>
+batch_size = 50
+
+def load_data(glove_dict):
+    """
+    Take reviews from text files, vectorize them, and load them into a
+    numpy array. Any preprocessing of the reviews should occur here. The first
+    12500 reviews in the array should be the positive reviews, the 2nd 12500
+    reviews should be the negative reviews.
+    RETURN: numpy array of data with each row being a review in vectorized
+    form"""
+    return data
+
+
+def load_glove_embeddings():
+    """
+    Load the glove embeddings into a array and a dictionary with words as
+    keys and their associated index as the value. Assumes the glove
+    embeddings are located in the same directory and named "glove.6B.50d.txt"
+    RETURN: embeddings: the array containing word vectors
+            word_index_dict: a dictionary matching a word in string form to
+            its index in the embeddings array. e.g. {"apple": 119"}
+    """
+    #data = open("glove.6B.50d.txt",'r',encoding="utf-8")
+    #if you are running on the CSE machines, you can load the glove data from here
+    #data = open("/home/cs9444/public_html/17s2/hw2/glove.6B.50d.txt",'r',encoding="utf-8")
+    return embeddings, word_index_dict
+
+
+def define_graph(glove_embeddings_arr):
+    """
+    Define the tensorflow graph that forms your model. You must use at least
+    one recurrent unit. The input placeholder should be of size [batch_size,
+    40] as we are restricting each review to it's first 40 words. The
+    following naming convention must be used:
+        Input placeholder: name="input_data"
+        labels placeholder: name="labels"
+        accuracy tensor: name="accuracy"
+        loss tensor: name="loss"
+
+    RETURN: input placeholder, labels placeholder, dropout_keep_prob, optimizer, accuracy and loss
+    tensors"""
+    dropout_keep_prob = tf.placeholder_with_default(1.0, shape=())
+
+    return input_data, labels, dropout_keep_prob, optimizer, accuracy, loss
--- hw2sent/train.py
+++ hw2sent/train.py
@@ -1,31 +1,77 @@
-<HTML>
-<HEAD>
-<TITLE>CGIWrap Error: Execution of this script not permitted</TITLE>
-<CENTER><H2>CGIWrap Error: Execution of this script not permitted</H2></CENTER>
-<HR><p></HEAD><BODY>
-Execution of (17s2/hw2/src/hw2sent/train.py) is not permitted
-for the following reason:
+"""
+You are encouraged to edit this file during development, however your final
+model must be trained using the original version of this file. This file
+trains the model defined in implementation.py, performs tensorboard logging,
+and saves the model to disk every 10000 iterations. It also prints loss
+values to stdout every 50 iterations.
+"""
 
-<P><DL><DT>Script is not executable. Issue 'chmod 755 filename'</DL>
-<P>
-<DL>
-<DT><B>Server Data:</B>
-<P>
-<DD><B>Server Administrator/Contact</B>: ss@cse.unsw.edu.au
-<DD><B>Server Name</B>: cgi.cse.unsw.edu.au
-<DD><B>Server Port</B>: 80
-<DD><B>Server Protocol</B>: HTTP/1.1
-<DD><B>Virtual Host</B>: cgi.cse.unsw.edu.au
-</DL>
-<P>
-<DL>
-<DT><B>Request Data:</B>
-<P>
-<DD><B>User Agent/Browser</B>: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.79 Safari/537.36
-<DD><B>Request Method</B>: GET
-<DD><B>Remote Address</B>: 129.94.8.246
-<DD><B>Remote Port</B>: 32324
-<DD><B>Referring Page</B>: http://www.cse.unsw.edu.au/~cs9444/17s2/hw2/src/hw2sent/
-</DL>
 
-</BODY></HTML>
+import numpy as np
+import tensorflow as tf
+from random import randint
+import datetime
+import os
+
+import implementation as imp
+
+batch_size = imp.batch_size
+iterations = 100000
+seq_length = 40  # Maximum length of sentence
+
+checkpoints_dir = "./checkpoints"
+
+def getTrainBatch():
+    labels = []
+    arr = np.zeros([batch_size, seq_length])
+    for i in range(batch_size):
+        if (i % 2 == 0):
+            num = randint(0, 12499)
+            labels.append([1, 0])
+        else:
+            num = randint(12500, 24999)
+            labels.append([0, 1])
+        arr[i] = training_data[num]
+    return arr, labels
+
+# Call implementation
+glove_array, glove_dict = imp.load_glove_embeddings()
+training_data = imp.load_data(glove_dict)
+input_data, labels, dropout_keep_prob, optimizer, accuracy, loss = \
+    imp.define_graph(glove_array)
+
+# tensorboard
+train_accuracy_op = tf.summary.scalar("training_accuracy", accuracy)
+tf.summary.scalar("loss", loss)
+summary_op = tf.summary.merge_all()
+
+# saver
+all_saver = tf.train.Saver()
+
+sess = tf.InteractiveSession()
+sess.run(tf.global_variables_initializer())
+
+logdir = "tensorboard/" + datetime.datetime.now().strftime(
+    "%Y%m%d-%H%M%S") + "/"
+writer = tf.summary.FileWriter(logdir, sess.graph)
+
+for i in range(iterations):
+    batch_data, batch_labels = getTrainBatch()
+    sess.run(optimizer, {input_data: batch_data, labels: batch_labels})
+    if (i % 50 == 0):
+        loss_value, accuracy_value, summary = sess.run(
+            [loss, accuracy, summary_op],
+            {input_data: batch_data,
+             labels: batch_labels})
+        writer.add_summary(summary, i)
+        print("Iteration: ", i)
+        print("loss", loss_value)
+        print("acc", accuracy_value)
+    if (i % 10000 == 0 and i != 0):
+        if not os.path.exists(checkpoints_dir):
+            os.makedirs(checkpoints_dir)
+        save_path = all_saver.save(sess, checkpoints_dir +
+                                   "/trained_model.ckpt",
+                                   global_step=i)
+        print("Saved model to %s" % save_path)
+sess.close()
